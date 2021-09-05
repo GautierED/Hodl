f075e767e61ecd1f7446eac04938560c55af2555
GautierED <gautierescallierduront@gmail.com>
2021-09-05 23:39:03 +0200

    replaced "the"n in async function by "await"

--- scripts/contract-interact-bsc.js
+++ scripts/contract-interact-bsc.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 Web3 = require('web3')
 
-const API_URL = process.env.API_BSC_TESTNET_URL;
+const API_URL = process.env.API_BSC_TESTNET;
 const PUBLIC_KEY = process.env.PUBLIC_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 const web3 = new Web3(API_URL);
 
+const { resolveProperties } = require('@ethersproject/properties');
 const contract = require("../artifacts/contracts/Hodl.sol/Hodl.json");
 const contractAddress = "0x7d1DB53B7e4f31d7018edcFA7045fD68F58A5175";
 const hodlContract = new web3.eth.Contract(contract.abi, contractAddress);
@@ -20,7 +21,8 @@ async function deposit(amount) {
         'value': web3.utils.toHex(web3.utils.toWei(amount, "ether"))
       };
 
-      signTheTransaction(tx);
+      const transactionReceipt = await signTheTransaction(tx);
+      console.log(transactionReceipt.blockNumber);
 }
 
 async function withdraw() {
@@ -31,7 +33,8 @@ async function withdraw() {
       'data': hodlContract.methods.withdraw().encodeABI()
     };
 
-    signTheTransaction(tx);   
+    const transactionReceipt = await signTheTransaction(tx); 
+    console.log(transactionReceipt);  
 }
 
 async function setTimeOfLock(time) {
@@ -42,46 +45,40 @@ async function setTimeOfLock(time) {
       'data': hodlContract.methods.setTimeOfLock(time).encodeABI()
     };
 
-    signTheTransaction(tx); 
+    const transactionReceipt = await signTheTransaction(tx); 
+    console.log(transactionReceipt);
 }
 
 async function signTheTransaction(tx) {
-  const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
-  signPromise.then((signedTx) => {
-    web3.eth.sendSignedTransaction(signedTx.rawTransaction, function(err, hash) {
-      if (!err) {
-        console.log("The hash of your transaction is: ", hash);
-        return hash;
-      } else {
-        console.log("Something went wrong when submitting your transaction:", err)
-      }
-    });
-  }).catch((err) => {
-    console.log("Promise failed:", err);
-  });
-}
+  try{
+    const signedTx = await web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
+    const hash = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+    return hash;
+  }
+  catch(err) {
+    return err;
+  }
+}  
 
 async function getEvent(hash, event) {
-  const receiptPromise = web3.eth.getTransactionReceipt(hash);
-  receiptPromise.then((receiptReceived) => {
-    hodlContract.getPastEvents(event, {fromBlock: receiptReceived.blockNumber, toBlock: receiptReceived.blockNumber}, function(err, event) {
-      if (!err) {
-        console.log(event);
-      } else {
-        console.log("Something went wrong", err);
-      }
-    })
-  })
+  try{
+    var receipt = await web3.eth.getTransactionReceipt(hash);
+    const events = await hodlContract.getPastEvents(event, {fromBlock: receipt.blockNumber, toBlock: receipt.blockNumber});
+    console.log(events);
+  }
+  catch(err) {
+    console.log("Something went wrong");
+  }
 }  
 
 async function main() {
     
     //await withdraw();
 
-    const message = await hodlContract.methods.balance().call();
-    console.log("Your balance is : " + message + " wei / " + (message/(10 ** 18)) + " ethereum");
+    //const message = await hodlContract.methods.balance().call();
+    //console.log("Your balance is : " + message + " wei / " + (message/(10 ** 18)) + " ethereum");
 
-    //await deposit("0.01");
+    await deposit("0.01");
 
     //await setTimeOfLock("3600");
 
--- scripts/contract-interact.js
+++ scripts/contract-interact.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const API_URL = process.env.API_URL;
+const API_URL = process.env.API_ROPSTEN;
 const PUBLIC_KEY = process.env.PUBLIC_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
