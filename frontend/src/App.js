fc5a8f8648f7918fa50494f2af7486deefa63991
GautierED <gautierescallierduront@gmail.com>
2021-09-07 14:20:23 +0200

    added the set time of lock and get time of unlock on the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,16 @@ import getWallet from './metamask.js';
 function App() {
   const [hodl, setHodl] = useState(undefined);
   const [balance, setBalance] = useState(undefined);
+  const [timeOfUnlock, setTimeOfUnlock] = useState(undefined);
 
   useEffect(() => {
     const init = async () => {
       const { hodl } = await getWallet();
       const balance = await hodl.balance();
+      const timeOfUnlock = await hodl.timeOfUnlock();
       setHodl(hodl);
       setBalance(balance);
+      setTimeOfUnlock(parseInt(timeOfUnlock._hex, 16));
     };
     init();
   }, []);
@@ -19,6 +22,19 @@ function App() {
     e.preventDefault();
     const balance = await hodl.balance();
     setBalance(balance);
+  };
+
+  const getTimeOfUnlock = async e => {
+    e.preventDefault();
+    const balance = await hodl.timeOfUnlock();
+    setBalance(balance);
+  };
+
+  const setTimeOfLock = async e => {
+    e.preventDefault();
+    const time = e.target.elements[0].value;
+    const tx = await hodl.setTimeOfLock(time);
+    await tx.wait();
   };
 
   if(
@@ -41,6 +57,40 @@ function App() {
           <h2>Get balance</h2>
           <form className="form-inline" onSubmit={e => getBalance(e)}>
             
+            <button 
+              type="submit" 
+              className="btn btn-primary"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+
+        <div className='col-sm-6'>
+          <h2>Time of unlock :</h2>
+          <p>{timeOfUnlock}</p>
+        </div>
+
+        <div className='col-sm-6'>
+          <h2>Get time of unlock</h2>
+          <form className="form-inline" onSubmit={e => getTimeOfUnlock(e)}>
+          <button 
+              type="submit" 
+              className="btn btn-primary"
+            >
+              Submit
+            </button>
+          </form>
+          </div>
+
+        <div className='col-sm-6'>
+          <h2>Set time of lock</h2>
+          <form className="form-inline" onSubmit={e => setTimeOfLock(e)}>
+            <input 
+              type="text" 
+              className="form-control" 
+              placeholder="Time of lock"
+            />
             <button 
               type="submit" 
               className="btn btn-primary"
