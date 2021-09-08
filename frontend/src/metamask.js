f2372c366a9574e3e0abe32684962d1a2b26a300
GautierED <gautierescallierduront@gmail.com>
2021-09-08 11:10:46 +0200

    verify the id of the network user is connected to require binance smart chain testnet

--- frontend/src/metamask.js
+++ frontend/src/metamask.js
@@ -3,25 +3,27 @@ import { ethers, Contract } from 'ethers';
 import Hodl from './Hodl.json';
 
 const contractAddress = "0x7d1DB53B7e4f31d7018edcFA7045fD68F58A5175";
+const binanceTestnetChainId = "97";
 
 const getWallet = () =>
-  new Promise( async (resolve, reject) => {
-    let provider = await detectEthereumProvider();
-    if(provider) {
-      await provider.request({ method: 'eth_requestAccounts' });
-      const networkId = await provider.request({ method: 'net_version' })
+new Promise( async (resolve, reject) => {
+  let provider = await detectEthereumProvider();
+  if(provider) {
+    var chainId = await provider.request({ method: 'eth_chainId' });
+    chainId = String(parseInt(chainId, 16));
+    if(chainId === binanceTestnetChainId) {
       provider = new ethers.providers.Web3Provider(provider);
       const signer = provider.getSigner();
       const hodl = new Contract(
-        //Hodl.networks[networkId].address,
         contractAddress,
         Hodl.abi,
         signer
       );
       resolve({hodl});
       return;
-    }
-    reject('Install Metamask');
-  });
+    } reject ('Switch Network');  
+  }
+  reject('Install Metamask');
+});
 
 export default getWallet;
